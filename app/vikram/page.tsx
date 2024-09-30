aca3259ee89990b048f5647beaada59dd1e5af24
Vikram Sethu Madhavan <vikramsethumadhavan@Vikrams-Mac-Studio.local>
2024-09-30 23:12:55 +0530

    first commit

---
+++ app/vikram/page.tsx
@@ -0,0 +1,9 @@
+// app/vikram/page.tsx
+
+export default function VikramPage() {
+    return (
+        <div>
+            <h1>Hello Vikram</h1>
+        </div>
+    );
+}
--- app/vikram/page/vikram.ts
+++
@@ -1,9 +0,0 @@
-// app/vikram/page.tsx
-
-export default function VikramPage() {
-  return (
-    <div>
-      <h1>Hello Vikram</h1>
-    </div>
-  );
-}
