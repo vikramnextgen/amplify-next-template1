46233ec20540efc9a7f6be505d72e89a00192af8
Vikram Sethu Madhavan <vikramsethumadhavan@Vikrams-Mac-Studio.local>
2024-09-30 22:21:18 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig =  {
+const nextConfig = {
   trailingSlash: false, // Disables automatic trailing slashes
+
+  async redirects() {
+    return [
+      {
+        source: '/api/simulate-error',  // Original path to redirect from
+        destination: '/vikram',         // New path to redirect to
+        permanent: false,               // Temporary redirect (302)
+      },
+    ]
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
